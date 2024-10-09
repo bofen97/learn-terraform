a98c807ed4733c8705365dc48878174fa9780dbd
bofeng1997 <bofeng1997@gmail.com>
2024-10-09 10:04:36 +0000

    fixed bug

--- modules/services/webserver-cluster/main.tf
+++ modules/services/webserver-cluster/main.tf
@@ -1,6 +1,4 @@
-provider "aws" {
-  region = "us-east-1"
-}
+
 terraform {
   backend "s3" {
     # Replace this with your bucket name!
@@ -13,45 +11,55 @@ terraform {
     encrypt        = true
   }
 }
+
+locals {
+  http_port    = 80
+  any_port     = 0
+  any_protocol = "-1"
+  tcp_protocol = "tcp"
+  all_ips      = ["0.0.0.0/0"]
+}
+
+
 data "terraform_remote_state" "db" {
   backend = "s3"
 
   config = {
     
-    bucket = "terraform-up-and-running-state-ty1997"
-    key  = "stage/data-stores/mysql/terraform.tfstate"
+    bucket = var.db_remote_state_bucket
+    key  = var.db_remote_state_key
     region = "us-east-1"
   }
 }
 
 
 resource "aws_security_group" "instance" {
-  name = "terraform-example-instance"
+  name = "${var.cluster_name}-instance"
 
   ingress {
     from_port   = var.server_port
     to_port     = var.server_port
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    protocol    = lcoal.tcp_protocol
+    cidr_blocks = local.all_ips
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = local.any_port
+    to_port     = local.any_port
+    protocol    = local.any_protocol
+    cidr_blocks = local.all_ips
   }
 }
 
 
 resource "aws_launch_template" "example" {
-  name_prefix   = "terraform-example-"
+  name_prefix   = "${var.cluster_name}-"
   image_id      = "ami-0fc5d935ebf8bc3bc"
-  instance_type = "t2.micro"
+  instance_type =  var.instance_type
 
   vpc_security_group_ids = [aws_security_group.instance.id]
 
-  user_data = base64encode(templatefile("user-data.sh", {
+  user_data = base64encode(templatefile("${path.module}/user-data.sh", {
     server_port = var.server_port
     db_address  = data.terraform_remote_state.db.outputs.address
     db_port     = data.terraform_remote_state.db.outputs.port
@@ -66,7 +74,7 @@ data "aws_vpc" "default" {
 }
 
 resource "aws_lb_target_group" "asg" {
-  name     = "terraform-asg-example"
+  name     = "${var.cluster_name}-asg"
   port     = var.server_port
   protocol = "HTTP"
   vpc_id   = data.aws_vpc.default.id
@@ -98,8 +106,8 @@ resource "aws_autoscaling_group" "example" {
   vpc_zone_identifier = data.aws_subnets.rds_subnets.ids
   target_group_arns   = [aws_lb_target_group.asg.arn]
   health_check_type   = "ELB"
-  min_size            = 2
-  max_size            = 10
+  min_size            = var.min_size
+  max_size            = var.max_size
 
   launch_template {
     id      = aws_launch_template.example.id
@@ -108,39 +116,39 @@ resource "aws_autoscaling_group" "example" {
 
   tag {
     key                 = "Name"
-    value               = "terraform-asg-example"
+    value               = var.cluster_name
     propagate_at_launch = true
   }
   depends_on = [data.aws_subnets.rds_subnets]
 }
 resource "aws_security_group" "alb" {
-  name = "terraform-example-alb"
+  name = "${var.cluster_name}-alb"
 
   # Allow inbound HTTP requests
   ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = local.http_port
+    to_port     = local.http_port
+    protocol    = local.tcp_protocol
+    cidr_blocks = local.all_ips
   }
 
   # Allow all outbound requests
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = local.any_port
+    to_port     = local.any_port
+    protocol    = local.any_protocol
+    cidr_blocks = local.all_ips
   }
 }
 resource "aws_lb" "example" {
-  name               = "terraform-asg-example"
+  name               = "${var.cluster_name}-asg"
   load_balancer_type = "application"
   subnets            = data.aws_subnets.rds_subnets.ids
   security_groups    = [aws_security_group.alb.id]
 }
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.example.arn
-  port              = 80
+  port              = local.http_port
   protocol          = "HTTP"
 
   # By default, return a simple 404 page
--- prod/services/webserver-cluster/main.tf
+++ prod/services/webserver-cluster/main.tf
@@ -5,4 +5,10 @@ provider "aws" {
 
 module "webserver_cluster" {
   source = "../../../modules/services/webserver-cluster"
+  cluster_name           = "webservers-prod"
+  db_remote_state_bucket = "terraform-up-and-running-state-ty1997"
+  db_remote_state_key    = "prod/data-stores/mysql/terraform.tfstate"
+  instance_type = "t2.micro"
+  min_size      = 2
+  max_size      = 10
 }
