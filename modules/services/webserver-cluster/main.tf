4d918a18e13d32b9e4cbeb334f0e4d4afc8fbc22
bofeng1997 <bofeng1997@gmail.com>
2024-10-09 18:17:10 +0800

    again

--- modules/services/webserver-cluster/main.tf
+++ modules/services/webserver-cluster/main.tf
@@ -1,16 +1,5 @@
 
-terraform {
-  backend "s3" {
-    # Replace this with your bucket name!
-    bucket         = "terraform-up-and-running-state-ty1997"
-    key            = "stage/services/webserver-cluster/terraform.tfstate"
-    region         = "us-east-1"
 
-    # Replace this with your DynamoDB table name!
-    dynamodb_table = "terraform-up-and-running-locks-ty1997"
-    encrypt        = true
-  }
-}
 
 locals {
   http_port    = 80
--- prod/services/webserver-cluster/main.tf
+++ prod/services/webserver-cluster/main.tf
@@ -1,3 +1,16 @@
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "terraform-up-and-running-state-ty1997"
+    key            = "prod/services/webserver-cluster/terraform.tfstate"
+    region         = "us-east-1"
+
+    # Replace this with your DynamoDB table name!
+    dynamodb_table = "terraform-up-and-running-locks-ty1997"
+    encrypt        = true
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
 }
